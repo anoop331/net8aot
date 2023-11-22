ec1944d4cfe1d0eabf88a0c20383fc0136c70529
Anoop Mathew <anoop.mathew@consultant.volvo.com>
2023-11-22 17:21:24 +0100

    initial

---
+++ AotLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AotLib;
+
+public static class NativeEntryPoints
+{
+
+    [UnmanagedCallersOnly(EntryPoint = "add")]
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+}
---
+++ LibTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LibTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("$Starting up the DLL interop");
+            var result = add(1, 8);
+            Console.WriteLine($"The sume of 1 and 8 is {result}");
+        }
+
+        [DllImport("aotlib.so")]
+        public static extern int add(int x, int y);
+    }
+}
